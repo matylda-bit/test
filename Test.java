a87cf49a2d95795152cdc1cc89e4ca486cdb24e0
Matylda M <m_matylda@hotmail.com>
2019-12-07 11:42:14 +0000

    Wykonalam zmiane

--- Test.java
+++ Test.java
@@ -1,7 +1,10 @@
 public class Test {
     public static void main (String [] args) {
         for (String s: args) {
+            for (String n: args)
             System.out.println (s);
+            System.out.print ("Matylda uczy sie gitowac");
+            System.out.println ("Wow");
 
         }
     }
